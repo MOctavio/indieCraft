bcb91cd9673aa477c06698d246b8c808438fb0bc
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-05 22:24:13 -0600

    fixed small sintaxis error

--- db/migrate/20151006041608_create_products.rb
+++ db/migrate/20151006041608_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string : title
+      t.string :title
       t.text :description
       t.string :image_url
       t.decimal :price, precision: 8, scale: 2
