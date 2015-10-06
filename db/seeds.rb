369055ac1980013f9969fb4b0a499d6f35834972
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-05 23:40:59 -0600

    Adde products seed, css, and images for products.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create!(title: 'Diesel Watches Ironside Chronograph Stainless Steel Watch',
+  description:
+    %{<p>
+        Featuring a dynamic chronograph dial and a coordinating bracelet,
+        the tonal Diesel Ironside watch delivers style with an edge.
+      </p>},
+  image_url:   'watch_01.jpg',
+  price: 325.00)
+# . . .
+Product.create!(title: 'Diesel Mens DZ4364 Ironside Analog Display Analog Quartz Green Watch',
+  description:
+    %{<p>
+        Renzo Rosso created the Diesel brand in 1978, choosing the name because it is easy to understand,
+        pronounced the same way around the world, and has a rugged feel about it; at that time, Diesel was
+        also considered an alternative energy so it was representative of an alternative taste in casual wear.
+        In 1985 Renzo Rosso acquired sole control of Diesel and surrounded himself with an international team of
+        talented designers and staff. Diesel launched its first For Successful Living advertising campaign in 1991,
+        instantly establishing a reputation for innovative marketing. The brands first flagship stores, in New York,
+        Rome and London, opened in 1996.
+      </p>},
+  image_url: 'watch_02.jpg',
+  price: 147.59)
+# . . .
+
+Product.create!(title: 'Diesel Mens DZ7353 Mini Daddy Biker Analog Display Analog Quartz Black Watch',
+  description:
+    %{<p>
+        <em>Lend your confident look a handsome distinction with this Diesel timepiece. Brushed stainless
+        steel case. Leather strap wit black-tone hardware accents. Adjustable buckle closure. Leather dial
+          with custom time display. Water resistant 5 ATM/50 meters. Imported.
+      </p>},
+  image_url: 'watch_03.jpg',
+  price: 225)
