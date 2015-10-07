a05d7f498437ff84edd982b5803d0f17bf880b4a
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-06 22:26:06 -0600

    fixed typo error on file extention

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true,
     format: {
-      with: %r{\.(git|jpg|png)\Z}i,
+      with: %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG iage.'
     }
 end
