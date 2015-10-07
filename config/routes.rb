b15baca819c09384cb4bd7c25ec99f84e1cb4217
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-06 22:38:04 -0600

    setted root page to sotre#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   resources :products
 
-
-  root 'store/index', as: 'store'
+  root 'store#index', as: 'store'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
