f1c2ebd4ca69e56fa46b904d02e2f124c0a56786
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-10 10:49:31 -0600

    enhanced controllers test

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#main-menu', minimun: 1
+    assert_select '.pure-menu-selected'
+    assert_select '.list_description p', minimun: 3
+    assert_select '.list_description .price', /\$[,\d]+\.\d\d/
   end
 
 end
