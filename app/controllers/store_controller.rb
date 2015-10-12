c8d22e3c019ba08d045d3fd47fe4a531d147794a
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-12 10:46:37 -0600

    added counter to Store/index view using session variable, just as show. Formatted and modified notice message for cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
+    increment_counter
     @products = Product.order(:title)
+  end
+
+  private
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
   end
 end
