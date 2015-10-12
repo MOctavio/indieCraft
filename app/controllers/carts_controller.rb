e5c58c95ebfc6b3370ab956a7479e04d1c67549d
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-12 14:17:34 -0600

    added total price for cart and included empty cart method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -28,7 +28,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
+        format.html { redirect_to @cart } #, notice: 'Cart was successfully created.' }
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new }
@@ -54,9 +54,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty.' }
       format.json { head :no_content }
     end
   end
