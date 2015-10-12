aae66948c3dcda7c7f018c907e8e2c89fe05a190
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-12 10:16:55 -0600

    created a cart and its line items related to products, also created a concern with a method to store the car in a session object for future requests

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,21 @@ class Product < ActiveRecord::Base
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG iage.'
     }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true;
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
+
 end
