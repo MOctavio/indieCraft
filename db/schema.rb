21f2d5e80150076c3ab44dad0277c14c706ee21a
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-10-12 12:59:05 -0600

    combined items in cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012173732) do
+ActiveRecord::Schema.define(version: 20151012182854) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
