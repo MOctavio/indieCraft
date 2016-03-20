200582713e21100a6a8fd3a8031b79060d6c0cda
Mario Octavio Jimenez <jimenez303@gmail.com>
2016-03-20 16:57:26 -0600

    seed database with new data according to images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 User.create! name: 'jarvis', password: 'secret', password_confirmation: 'secret'
 
 Product.delete_all
-Product.create!(title: 'Wooden Doormat',
+Product.create!(title: 'Leather Bracelet',
                 description:
     %(<p>
         Sumo omnium placerat ut qui, et vidit maluisset omittantur ut vim.
       </p>),
                 image_url:   'article-1-300x208.jpg',
-                price: 46.50)
+                price: 90.50)
 # . . .
-Product.create!(title: 'Hobby Horse',
+Product.create!(title: 'Handmade Silver Ring',
                 description:
     %(<p>
         Ex sit tale ludus reprimique, tibique nominavi nam ad, cibo repudiare usu ne.
       </p>),
                 image_url: 'article-2-300x208.jpg',
-                price: 13.00)
+                price: 750.00)
 # . . .
-Product.create!(title: 'Picture Frame',
+Product.create!(title: 'Mens Tide',
                 description:
     %(<p>
         Vix te vidisse delicatissimi. Ludus impedit vim at, quod scaevola ne pro.
@@ -26,10 +26,10 @@ Product.create!(title: 'Picture Frame',
                 price: 24.00)
 
 # . . .
-Product.create!(title: 'Statue',
+Product.create!(title: 'Leather Wallet',
                 description:
   %(<p>
       Vix te sumo omnium placerat ut qui, quod scaevola ne pro tibique nominavi.
     </p>),
                 image_url: 'article-4-300x208.jpg',
-                price: 13.00)
+                price: 135.00)
